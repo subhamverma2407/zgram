5a120a39baa73c07e134ada9641bb09a7d98217f
Subhamverma2407 <subhamverma2407@gmail.com>
2021-11-06 01:19:07 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+import Header from "./components/Header/Header";
+
+const Main = styled.div`
+  width: 70%;
+  margin: auto;
+  background: green;
+
+  top: 5rem;
+  position: relative;
+  z-index: -1;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <Main></Main>
+    </>
   );
 }
 
